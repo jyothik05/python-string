3cc06505949d16d1cd5acb34dbcaaf1d6362fec8
jyothik05 <jyothikaswamykore@gmail.com>
2024-08-23 16:25:00 +0530

    Add files via upload

---
+++ String.py
@@ -0,0 +1,22 @@
+'''string is a group of character enclosed in single or double qoutes
+    immutable
+    '''
+str = "python"
+print(str)
+
+
+#STRING ACCESS/STRING INDEXING
+'''string can be accessed in two directions
+--->forward index (left to right)   starts from 0 to n-1
+--->backward index (right ti left)   starts from -1 to -n'''
+
+
+str = "python"
+print(str[2])
+print(str[-1])
+#print(str[6])
+
+
+str = "python"
+str[4]="l"
+print(str)
